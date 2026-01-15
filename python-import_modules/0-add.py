988f016b5c8af09b457c29a4e645bfda1cde1a6c
csrama <ramadafer1@gmail.com>
2026-01-15 07:02:17 +0000

    add function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""0-add: Import add function"""
+
+from add_0 import add
 
 if __name__ == "__main__":
-    from add_0 import add
-   
     a = 1
     b = 2
-    
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
