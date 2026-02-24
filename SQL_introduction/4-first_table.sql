6506b20fe3bdb8b8e7413cd600ff952a6d8f06df
csrama <ramadafer1@gmail.com>
2026-02-24 09:44:31 +0000

    first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- 4. First table
+-- Creates table first_table in the current database (does not fail if it already exists).
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
