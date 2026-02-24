58d18d644ba9e038e787701c31cbd43803adba76
csrama <ramadafer1@gmail.com>
2026-02-24 09:54:58 +0000

    11 best score

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- 11. Select the best
+-- Lists all records from second_table with score >= 10 ordered by score (highest first).
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
